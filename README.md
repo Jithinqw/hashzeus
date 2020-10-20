64cbacac71af3db88423c8f794754e54bf8295cc
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2020-10-20 19:11:17 +0530

    Adding hashzeus

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # hashzeus
-generate globally unique event Ids
+
+Generate globally unique eventId based on microservices on a network. This is created as an alternative for Javascript timestamps for identifying services on a network.
+
+# Installation
+
+```npm install hashzeus --save```
+
+# Usage
+
+```javascript
+    var hashZeus = require('hashzeus');
+    var demo = new hashZeus()
+    console.log(demo.generateHash('zerodha'));
+    //apple-6dylVZFUl4cP3fH6bKRe.G
+    console.log(demo.generateHash('byjus'));
+    //byjus-4SFUv0vu06psuKOUYiw18T
+    var demo1 = new hashZeus();
+    console.log(demo1.generateHash('byjus')); 
+    // generates completely different Id from a differnt object.
+    //byjus-3s4REK77l2Fsh2hE8EIUGT
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const d64 = require('d64'),
+    uuid = require('uuid');
+
+class hashZeus {
+    b = Uint8Array;
+    constructor() {
+        this.b = new Uint8Array(24);
+        uuid.v4(null, this.b, 8);
+    }
+
+    /**
+     * @function appendServiceName
+     * @description strips ... and adds service name
+     * @param {*} name 
+     * @param {*} eventId 
+     */
+    appendServiceName(name, eventId) {
+        let tempSer = eventId.substring(10);
+        return name + "-" + tempSer ;
+    }
+    
+    /**
+     * @function generateHash
+     * @description generates eventId based on uuid and d64
+     * @param {*} serviceName 
+     * @returns {*} string
+     */
+    generateHash(serviceName) {
+        for(let i=7; i>=0; i--) {
+            if(this.b[i] !== 255) {
+                this.b[i]++;
+                break;
+            }
+            this.b[i] = 0;
+        }
+        let hashwithoutName = d64.encode(this.b);
+        if(serviceName) {
+            return this.appendServiceName(serviceName, hashwithoutName);
+        }
+        return hashwithoutName;
+    }
+}
+
+module.exports = hashZeus;
